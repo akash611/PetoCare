b99f31e8cf6c788b5b1441f40fbcbcc01b7bff75
Akash Pal <akash.majida@gmail.com>
2022-04-23 00:22:35 +0530

    added bootstarp and make the structure more clear

--- petocare/urls.py
+++ petocare/urls.py
@@ -20,6 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("pages.urls")),
     path("login/", include("login.urls")),
-    path("signup/", include("login.urls")),
+    path("signup/", include("signup.urls")),
 
 ]
