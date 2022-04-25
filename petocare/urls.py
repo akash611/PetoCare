cb075fc15e45a4ae5413bf0a911894fef5307252
Akash Pal <akash.majida@gmail.com>
2022-04-25 10:35:22 +0530

    made the pages

--- petocare/urls.py
+++ petocare/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from main_app import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name="home_page"),
+    path('',include('main_app.urls')),
 ]
