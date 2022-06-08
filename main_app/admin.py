855750086730aee2ee0022293c28a2e96e9e5d1d
Kaustav Biswas <kostavbiswas1000@gmail.com>
2022-06-09 01:56:43 +0530

    Created the Database Succesfully

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from main_app.models import Petparent, Groomer, Vet, Services, VetBooking, ServiceBooking
+
+admin.site.register(Petparent)
+admin.site.register(Groomer)
+admin.site.register(Vet)
+admin.site.register(Services)
+admin.site.register(VetBooking)
+admin.site.register(ServiceBooking)
